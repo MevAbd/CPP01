0935b81e069ea1892f5804505996ded9834753c2
Maeva ALBRAND <malbrand@e1r4p1.clusters.42paris.fr>
2022-07-06 22:15:07 +0200

    Ajout header

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 21:51:58 by malbrand          #+#    #+#             */
+/*   Updated: 2022/07/06 22:08:19 by malbrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sstream>
 #define AC 0
-#define IN 1
-#define OUT 2
+#define FILES 1
 
 void	replace(std::fstream &file, std::string s1, std::string s2, std::string line)
 {
@@ -11,6 +24,7 @@ void	replace(std::fstream &file, std::string s1, std::string s2, std::string lin
 	pos = line.find(s1);
 	if (pos == std::string::npos)
 	{
+		std::cout << pos << std::endl;
 		file << line;
 		return ;
 	}
@@ -27,31 +41,38 @@ void	print_error(int error)
 {
 	if (error == AC)
 		std::cout << "Wrong number of arguments" << std::endl;
-	if (error == IN)
-		std::cout << "Error file in" << std::endl;
-	if (error == OUT)
-		std::cout << "Error file out" << std::endl;
+	if (error == FILES)
+		std::cout << "Error file" << std::endl;
 }
 
 int	main(int ac, char **av)
 {
 	if (ac != 4)
+	{
 		print_error(AC);
+		return (1);
+	}
 
 	std::string	name = av[1];
 	std::fstream	file;
 	file.open (av[1], std::fstream::in);
-	if (!file)
-		print_error(IN);
+	if (!file.is_open())
+	{
+		print_error(FILES);
+		return (1);
+	}
 
 	std::string	s1 = av[2];
 	std::string	s2 = av[3];
 	std::string	ret = name + ".replace";
 	std::fstream	file_ret;
 
-	file_ret.open(ret, std::fstream::out | std::fstream::app);
-	if (!file_ret)
-		print_error(OUT);
+	file_ret.open(ret.c_str(), std::fstream::out | std::fstream::app);
+	if (!file_ret.is_open())
+	{
+		print_error(FILES);
+		return (1);
+	}
 
 	std::string	line;
 	while (getline(file, line))
